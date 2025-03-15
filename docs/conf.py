b7bb95c1cbb13377cd6acde91eb3765f0d2bf00e
Philip Monaco <philip.monaco@berkeley.edu>
2025-03-15 17:05:57 -0400

    Update project structure and add auto docs

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../src"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Mo Data'
+copyright = '2025, Group'
+author = 'Group'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.inheritance_diagram",
+    "sphinx.ext.graphviz",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autosummary",   
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
+html_theme_options = {
+    "collapse_navigation": False,  # keep the navigation expanded
+    "sticky_navigation": True,     # optionally, make the sidebar sticky as you scroll
+    "navigation_depth": 1,         # adjust the depth as needed
+}
---
+++ docs/index.rst
@@ -0,0 +1,40 @@
+.. Mo Data documentation master file, created by
+   sphinx-quickstart on Sat Mar 15 14:19:41 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Mo Data documentation
+=====================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+API Reference
+=============
+
+mo_sno.models
+--------------
+.. automodule:: mo_sno.models
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mo_sno.pipeline
+----------------
+.. automodule:: mo_sno.pipeline
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+mo_sno.preprocessing
+---------------------
+.. automodule:: mo_sno.preprocessing
+   :members:
+   :undoc-members:
+   :show-inheritance:
